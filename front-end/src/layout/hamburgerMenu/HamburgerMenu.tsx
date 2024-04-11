a5e05ca45fd423854337a681e5572824bc748767
JJHyeok13 <jjinh99@naver.com>
2024-04-11 16:29:40 +0900

    fix: 스크립트 페이지 주소 변경

--- front-end/src/layout/hamburgerMenu/HamburgerMenu.tsx
+++ front-end/src/layout/hamburgerMenu/HamburgerMenu.tsx
@@ -24,7 +24,7 @@ const HamburgerMenu: React.FC<{ handleHamburgerMenu: () => void }> = ({
       <styles.ButtonContainer>
         <HamburgerMenuButton buttonName="변환하기" address="/convert" />
         <HamburgerMenuButton buttonName="요약하기" address="/summarize" />
-        <HamburgerMenuButton buttonName="저장된 스크립트" address="/script" />
+        <HamburgerMenuButton buttonName="저장된 스크립트" address="/myscript" />
         <HamburgerMenuButton buttonName="마이페이지" address="/mypage" />
       </styles.ButtonContainer>
     </styles.Container>
